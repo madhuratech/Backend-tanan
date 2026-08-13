50aade9216713f633ae7c58fca8c5d4c7ef4f313
keerthi-1802 <kiruthikanarayanan002@gmail.com>
2026-08-13 11:06:31 +0530

    remove logs

--- config/db.js
+++ config/db.js
@@ -3,10 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_NAME:", process.env.DB_NAME);
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
